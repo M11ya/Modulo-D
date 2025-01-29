5ec94e269788ef3c985053ad8f9ff7e46d33717b
Eduardo Gabriel Freire <duduagaguiar@gmail.com>
2025-01-29 19:20:45 -0300

    Atividades do  Segundo Dia de Aula

--- exercício03.php
+++ exercício03.php
@@ -1,36 +1,34 @@
 <?php
+$num1 = readline("Digite o primeiro número: ");
+$num2 = readline("Digite o segundo número: ");
+$operacao = readline("Digite a operação (soma, subtração, multiplicacao, divisao): ");
+$resultado = '';
 
-    $num1;
-    $num2;
-    $operacao;
-    $resultado;
-
-
-    
-    if (isset($num1, $num2, $operacao) && is_numeric($num1) && is_numeric($num2)) {
-        switch ($operacao) {
-            case "soma":
-                $resultado = $num1 + $num2;
-                break;
-            case "subtraço":
-                $resultado = $num1 - $num2;
-                break;
-            case "multiplicacao":
-                $resultado = $num1 * $num2;
-                break;
-            case "divisao":
-                if ($num2 != 0) {
-                    $resultado = $num1 / $num2;
-                } else {
-                    $resultado = "Erro: Divisão por zero!";
-                }
-                break;
-            default:
-                $resultado = "Operação inválida";
-                break;
-        }
-    } else {
-        $resultado = "Por favor, insira números válidos.";
+if (isset($num1, $num2, $operacao) && is_numeric($num1) && is_numeric($num2)) {
+    switch ($operacao) {
+        case "soma":
+            $resultado = $num1 + $num2;
+            break;
+        case "subtração":
+            $resultado = $num1 - $num2;
+            break;
+        case "multiplicacao":
+            $resultado = $num1 * $num2;
+            break;
+        case "divisao":
+            if ($num2 != 0) {
+                $resultado = $num1 / $num2;
+            } else {
+                $resultado = "Erro: Divisão por zero!";
+            }
+            break;
+        default:
+            $resultado = "Operação inválida";
+            break;
     }
+} else {
+    $resultado = "Por favor, insira números válidos.";
+}
 
+echo $resultado;
 ?>
